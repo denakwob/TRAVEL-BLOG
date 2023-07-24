223eede10062ea9969f7bfee1462148f978ca39b
denakwob <125455377+denakwob@users.noreply.github.com>
2023-07-24 09:28:43 +0000

    Add response to newsletter

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,18 @@ function animateElementsOnScroll() {
 }
 
 window.addEventListener("scroll", animateElementsOnScroll);
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwySXHtRsvhcDSYeKTbm7V5g-C0TIv_FnJ1Zaw_NSF1DazC96MRGSERlI1RW-u7NVql/exec'
+      const form = document.forms['submit-to-google-sheet']
+    
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => console.log('Success!', response))
+          .catch(error => console.error('Error!', error.message))
+      })
+    
+      window.onload = function() {
+        var heroContent = document.querySelector('.hero-content');
+        heroContent.classList.add('show');
+      };
